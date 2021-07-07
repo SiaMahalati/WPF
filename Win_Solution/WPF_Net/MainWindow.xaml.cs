ef4a1434ebfcf1baf696226c159c258608c86f58
Sia Mahalati <siamahalati@gmail.com>
2021-07-07 12:50:24 -0400

    Added WPF_461 project

--- Win_Solution/WPF_Net/MainWindow.xaml.cs
+++ Win_Solution/WPF_Net/MainWindow.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,5 +27,55 @@ namespace WPF_Net
         {
             InitializeComponent();
         }
+
+        DataSet ds = new DataSet();
+        
+
+   
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Save_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Next_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Previous_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        public DataSet GetEmployeesList()
+        {
+            DataSet dsEmployee = new DataSet();
+            using (var con = new SqlConn(connectionString))
+            {
+                SqlCommand objSqlCommand = new SqlCommand("GetEmployeeList", con)
+                {
+                    CommandType = CommandType.StoredProcedure
+                };
+                SqlDataAdapter objSqlDataAdapter = new SqlDataAdapter(objSqlCommand);
+                try
+                {
+                    objSqlDataAdapter.Fill(dsEmployee);
+                    dsEmployee.Tables[0].TableName = "Employees";
+                    grvEmployee.DataSource = dsEmployee;
+                    grvEmployee.DataBind();
+                }
+                catch (Exception ex)
+                {
+                    return dsEmployee;
+                }
+            }
+            return dsEmployee;
+        }
+
     }
 }
